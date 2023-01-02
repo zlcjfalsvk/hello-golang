06ccb0ee8b4574617c70e5003465e86d510504e9
cheolmin choi <zl0759vk@naver.com>
2023-01-02 17:26:02 +0900

    study go

---
+++ main.go
@@ -0,0 +1,32 @@
+// go 는 main package로 부터 실행 함
+package main
+
+import "fmt"
+
+// factored import === import "fmt"
+
+func main() {
+	// PrintLn 은 fmt package 에서 export 된 함수이다
+	// 이처럼 go 에서는 export 하기 위해선 함수 함수의 맨 앞자를 대문자로 정의 해야 export
+	fmt.Println("Hello Golang")
+	fmt.Println(Add(1, 2, 3))
+
+	// double quotes
+	const x = "ab\t" // const 는 := 사용 불가능
+
+	// back quotes - 이스케이프 허용 안함
+	y := `d\t` // var string y = "d"
+
+	// single quotes - (byte(ascii) === uinit8) 또는 (rune(utf-8) === int32) 타입 선언 할 때
+	var z byte
+	z = 'a' // 97
+
+	// defer 예약어, defer 예약어를 가진 함수가 종료 될 때까지 대기 하다 마지막에 호출됨, defer가 여러개일 경우 stack에 쌓임
+	defer fmt.Println(Strings(x, y, z))
+	fmt.Printf("x is of type %T \n", x)
+	defer fmt.Println(Strings("a", "b", 'c')) // 스택으로 인해 먼저 호출
+
+	//Gugudan(9)
+	//fmt.Println(Sqrt(2))
+
+}
---
+++ study.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+func Add(x, y, z int) int { // type이 같을 경우 param 마다의 type 선언을 생략할 수 있음 => func add(x int, y int, z int)
+	return x + y + z
+}
+
+func Strings(x, y string, z byte) (string, string, float64) {
+	return x, y, float64(z)
+}
+
+// Split
+// naked return return value가 없지만 return type 에 변수가 정의 됨
+// 짧은 함수에서 사용되어야 가독성이 좋음
+func Split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+func Gugudan(dan int) {
+	// for 처럼 ; 이용 하여 여러가지 구문 가능
+	if dan == 0 {
+		dan = 9
+	}
+
+	//for i := 1; i <= dan; i++ {
+	//	for j := 1; j <= 9; j++ {
+	//		fmt.Printf("%d X %d = %d \n", i, j, i*j)
+	//	}
+	//}
+
+	i := 1
+	j := 1
+	for ; i <= dan; i++ {
+		// go 는 다음과 같이 ;를 생략할 수 있기 때문에 while 예약어 없음
+		// for {} === while(true) {}
+		for j <= 9 {
+			fmt.Printf("%d X %d = %d \n", i, j, i*j)
+			j++
+		}
+		j = 1
+	}
+}
+
+func Sqrt(x float64) float64 {
+	z := x / 2
+	for i := 1; i <= 10; i++ {
+		z -= (z*z - x) / (2 * z)
+		fmt.Printf("Sqrt approximation of %v attempt %v = %v\n", x, i, z)
+	}
+	return z
+}
